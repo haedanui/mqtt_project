bfe6f4c30037f0440eca01c50af12198e2f469bf
badgelatte <saucy5939@gmail.com>
2023-11-29 15:28:56 +0900

    [feat] MqttInNode 완성

--- src/main/java/com/nhnacademy/node/MqttInNode.java
+++ src/main/java/com/nhnacademy/node/MqttInNode.java
@@ -1,29 +1,71 @@
 package com.nhnacademy.node;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.json.JSONObject;
 
 import com.nhnacademy.Output;
 import com.nhnacademy.Wire;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 public class MqttInNode extends Node implements Output {
     private final Set<Wire> outWires = new HashSet<>();
 
     @Override
     public void wireOut(Wire wire) {
-        // TODO Auto-generated method stub
+        outWires.add(wire);
     }
 
     @Override
     public void process() {
-        // TODO Auto-generated method stub
+        String id = UUID.randomUUID().toString();
+
+        try (IMqttClient client = new MqttClient("tcp://ems.nhnacademy.com:1883", id)) {
+            MqttConnectOptions options = new MqttConnectOptions();
+
+            // options.setAutomaticReconnect(true);
+
+            client.connect();
+            for (Wire wire : outWires) {
+                client.subscribe("#", (topic, msg) -> {
+                    JSONObject object = new JSONObject();
+                    object.put("topic",topic);
+                    object.put("payload", msg);
+                    wire.getBq().add(object);
+                    log.info("topic : {}\n payload : {}", topic, msg);
+                });
+            }
+            
+            while(!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(100);
+            }
+
+            client.disconnect();
+        } catch (Exception e) {
+            log.info("{}", e);
+        }
     }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        process();
+
     }
+    
+    /* public static void main(String[] args) {
+        MqttInNode mqttInNode = new MqttInNode();
+        Wire wire = new Wire();
+        mqttInNode.wireOut(wire);
+        mqttInNode.run();
+    } */
 }
