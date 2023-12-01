acbf443d31faf9c9fc691c0643e715f348156e7e
AoiTuNa <bestspark123@naver.com>
2023-12-01 11:41:44 +0900

    feat/preprocess 구현

--- src/main/java/com/nhnacademy/function/Preprocess.java
+++ src/main/java/com/nhnacademy/function/Preprocess.java
@@ -13,10 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Preprocess implements Executable {
     // TODO rename wild card
-    private static final String a = "#";
-    private static final String b = "+";
+    private static final String WIDECARD_ALL = "#";
+    private static final String WIRDCARD_SINGLE = "+";
 
     private static boolean enableTopic(String filter, final String topic) {
+        /**
+         * 토픽이 필터로 설정한 값과 같다면 true를 반환해 다음 단계를 처리하게 한다.
+         */
         if (topic == null || filter == null) throw new IllegalArgumentException();
 
         var topicList = topic.split("/");
@@ -24,13 +27,36 @@ public class Preprocess implements Executable {
 
         int len = Math.min(topicList.length, filterList.length);
         for (int i = 0; i < len; ++i) {
-            if (filterList[i].equals(a)) return false;
-
-            if (!filterList[i].equals(topicList[i]) && !filterList[i].equals(b)) return true;
-            if (filterList[i].equals(b) && i + 1 == len && filterList.length == len && topicList.length != filterList.length) return true;
+            if (filterList[i].equals(WIDECARD_ALL)) return true;
+            if (!filterList[i].equals(topicList[i]) && !filterList[i].equals(WIRDCARD_SINGLE)) return false;
+            if (filterList[i].equals(WIRDCARD_SINGLE) && i + 1 == len && filterList.length == len && topicList.length != filterList.length) return false;
         }
 
+        return true;
+    }
+
+    private static boolean enableSensor(String filter, final JSONObject target){
+        /**
+         * 지정된 양식 속 원하는 데이터 값이 있는지 확인하고 있다면 true를 반환해 다음 단계를 처리하게 한다.
+         */
+        var filterList = filter.split(",".trim());
+        if(target.has("payload")){
+            JSONObject payload = target.getJSONObject("payload");
+            if(payload.has("object")){
+                JSONObject object = payload.getJSONObject("object");
+                int count = 0;
+                for(int i = 0; i < filterList.length; i++){
+                    if(object.has(filterList[i])){
+                        count ++;
+                    }
+                }
+                if(count >= 1){
+                    return true;
+                }
+            }
+        }
         return false;
+
     }
 
     @Override
@@ -42,9 +68,10 @@ public class Preprocess implements Executable {
                 if (!messageQ.isEmpty()) {
                     JSONObject msg = messageQ.poll();
                     
-                    boolean shouldFilter = enableTopic(Config.getCurrentConfig().getString("an"), msg.getString("topic"));
+                    boolean allowedTopic = enableTopic(Config.getCurrentConfig().getString("applicationName"), msg.getString("topic"));
+                    boolean allowedSensor = enableSensor(Config.getCurrentConfig().getString("allowedSensor"), msg);
                     
-                    if (!shouldFilter) {
+                    if (allowedTopic&&allowedSensor) {
                         log.info(msg.getString("topic"));
                         for (Wire outWire : outWires) {
                             outWire.getBq().put(msg);
