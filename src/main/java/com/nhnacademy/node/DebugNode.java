6fd66bc06c4f889b2004b1cc3fd4e5f08ad14657
haedanui <rhs9705kr@naver.com>
2023-12-01 11:24:48 +0900

    debugnode 새로운 구조에 맞게 작성

--- src/main/java/com/nhnacademy/node/DebugNode.java
+++ src/main/java/com/nhnacademy/node/DebugNode.java
@@ -16,23 +16,31 @@ import lombok.extern.slf4j.Slf4j;
 public class DebugNode extends ActiveNode implements Input {
     private Set<Wire> inputWires = new HashSet<>();
 
+    /**
+     * 와이어를 inputWire Set 에 넣는다.
+     * 그러면 inputWire를 통해 해당 노드에 연결된 와이어에 있는 Blocking Queue에 접근 할 수있다.
+     */
     @Override
     public void wireIn(Wire wire) {
         inputWires.add(wire);
     }
 
+    /**
+     * input와이어에 있는 큐가 비어있는 동안 메시지를 출력하지 않다가 큐에 메시지가 들어오면 해당 메시지를 로그로 출력함
+     */
     @Override
     public void process() {
         try {
-            for (Wire wire : inputWires) {
-                var messageQ = wire.getBq();
-
-                JSONObject msg = messageQ.poll();
-
+            for (Wire wires : inputWires) {
+                if (wires.getBq().isEmpty()) {
+                    continue;
+                }
+                JSONObject msg = wires.getBq().poll();
                 log.info(msg.toString());
             }
-            
+
         } catch (Exception ignore) {
+            log.error(ignore.getMessage());
         }
     }
 }
