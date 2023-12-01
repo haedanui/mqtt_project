12cf8e20938670fb06d633005577f5de9cfb339c
badgelatte <saucy5939@gmail.com>
2023-12-01 11:44:58 +0900

    완성

--- src/main/java/com/nhnacademy/node/MqttInNode.java
+++ src/main/java/com/nhnacademy/node/MqttInNode.java
@@ -2,8 +2,6 @@ package com.nhnacademy.node;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
-
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.json.JSONObject;
@@ -13,58 +11,106 @@ import com.nhnacademy.Wire;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-
-@Getter
+    
 @Slf4j
+@Getter
 public class MqttInNode extends ActiveNode implements Output {
     private static final String DEFAULT_URI = "tcp://ems.nhnacademy.com:1883";
+    private static final String DEFAULT_TOPIC = "#";
 
     private final Set<Wire> outWires = new HashSet<>();
-    private String uri;
-
-    private IMqttClient client;
-
+    private final String uri;
+    private String fromTopic;
+    
+    /*
+     * 기본 URI = tcp://ems.nhnacademy.com:1883
+     */
     public MqttInNode() {
-        this(DEFAULT_URI);
+        this(DEFAULT_URI, DEFAULT_TOPIC);
     }
 
+    /*
+     * 사용자 지정 constructors
+     */
     public MqttInNode(String uri) {
-        this.uri = uri;
+        this(uri, DEFAULT_TOPIC);
     }
 
+    public MqttInNode(String uri, String topic) {
+        this.uri = uri;
+        fromTopic = topic;
+    }
+
+    /*
+     * topic 설정하기
+     */
+    public void setTopic(String topic){
+        fromTopic = topic;
+    }
+
+    /*
+     * wire 연결
+     */
     @Override
     public void wireOut(Wire wire) {
         outWires.add(wire);
     }
 
+    /*
+     * id를 가져올 수 있도록 한다.
+     */
+    public String getMqttId() {
+        return getId().toString();
+    }
+
+    /*
+     * 들어오는 모든 data를 연결된 wire에 넣어준다
+     */
     @Override
     public void process() {
-    }
-
-    @Override
-    public void preprocess() {
-        String clientId = UUID.randomUUID().toString();
-        try {
-            client = new MqttClient(uri, clientId);
+        try (IMqttClient client = new MqttClient(uri, getMqttId())){
             client.connect();
 
-            client.subscribe("applictaion/#", (topic, payload) -> {
-                log.info("payload {}", payload.toString());
-                JSONObject jsonObject = new JSONObject();
+            client.subscribe("application/#", (topic, msg) -> {
+                JSONObject object = new JSONObject();
 
-                jsonObject.put("topic", topic);
-                jsonObject.put("payload", new JSONObject(payload.toString()));
+                log.info("dddddd - {}",msg);
+                object.put("topic", new JSONObject(topic));
+                object.put("payload", new JSONObject(msg.toString()));
 
-                log.info("asdasd {}", jsonObject);
+                log.info("object: {}",object);
                 
                 for (Wire wire : outWires) {
-                    var messageQ = wire.getBq();
+                    
+                    // String sTopic = "\""+topic + "\"";
+                    // String sMsg = "\""+ new String(msg.getPayload()) + "\"";
 
-                    messageQ.put(jsonObject);
+                    // JSONObject jsonTopic = new JSONObject("{\"topic\":\""+topic+"\", \"payload\":\""+new String(msg.getPayload())+"\"}");
+                    // JSONObject object = new JSONObject();
+                    // object.put("topic", sTopic);
+                    // object.put("payload", sMsg);
+
+                    wire.getBq().add(object);
+                    // log.info("object: {}",jsonTopic);
+                    
+
                 }
             });
-        } catch (Exception ignore) {
-            log.warn(ignore.getMessage());
+            while(!Thread.currentThread().isInterrupted()){
+                Thread.sleep(100);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            Thread.currentThread().interrupt();
         }
+
     }
+
+    public static void main(String[] args) {
+        MqttInNode mqttInNode = new MqttInNode();
+        Wire wire = new Wire();
+        mqttInNode.wireOut(wire);
+        mqttInNode.start();
+    }
+
 }
