27c9a989e1d5ad491e378f943d59f4ee7cb99c9e
jki12 <sjak0227@naver.com>
2023-12-02 12:52:39 +0900

    [fix] 코드 로직 수정, 임시 저장 (아직 테스트 안했음)

--- src/main/java/com/nhnacademy/function/Preprocess.java
+++ src/main/java/com/nhnacademy/function/Preprocess.java
@@ -12,52 +12,51 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Preprocess implements Executable {
-    // rename wild card
     private static final String WIDECARD_ALL = "#";
-    private static final String WIRDCARD_SINGLE = "+";
+    private static final String WILDCARD_ANY = "+";
 
+    /**
+     * 토픽이 필터로 설정한 값과 같다면 true를 반환해 다음 단계를 처리하게 한다.
+     */
     private static boolean enableTopic(String filter, final String topic) {
-        /**
-         * 토픽이 필터로 설정한 값과 같다면 true를 반환해 다음 단계를 처리하게 한다.
-         */
         if (topic == null || filter == null) throw new IllegalArgumentException();
 
         var topicList = topic.split("/");
         var filterList = filter.split("/");
 
+        boolean temp = false; // 필터의 마지막 값이 WILDCARD_ANY인지 확인하는 변수
         int len = Math.min(topicList.length, filterList.length);
         for (int i = 0; i < len; ++i) {
             if (filterList[i].equals(WIDECARD_ALL)) return true;
-            if (!filterList[i].equals(topicList[i]) && !filterList[i].equals(WIRDCARD_SINGLE)) return false;
-            if (filterList[i].equals(WIRDCARD_SINGLE) && i + 1 == len && filterList.length == len && topicList.length != filterList.length) return false;
+            if (!filterList[i].equals(topicList[i]) && !filterList[i].equals(WILDCARD_ANY)) return false;
+
+            temp = (filterList[i].equals(WILDCARD_ANY));
         }
 
-        return true;
+        return !(temp && filterList.length != topicList.length);
     }
 
-    private static boolean enableSensor(String filter, final JSONObject target) {
-        if (filter.equals("all")) return true;
-        /**
-         * 지정된 양식 속 원하는 데이터 값이 있는지 확인하고 있다면 true를 반환해 다음 단계를 처리하게 한다.
-         */
-        var filterList = filter.split(",".trim());
-        if(target.has("payload")){
-            JSONObject payload = target.getJSONObject("payload");
-            if(payload.has("object")){
+    /**
+     * 지정된 양식 속 원하는 데이터 값이 있는지 확인하고 하나라도 있다면 true를 반환해 다음 단계를 처리하게 한다.
+     */
+    private static boolean enableSensor(String allowedSensor, final JSONObject payload) {
+        if (allowedSensor.equals("all")) return true;
+
+        var allowedSeneorList = allowedSensor.split(",");
+
+        // if(target.has("payload")) {
+            // JSONObject payload = target.getJSONObject("payload");
+
+            if(payload.has("object")) {
                 JSONObject object = payload.getJSONObject("object");
-                int count = 0;
-                for(int i = 0; i < filterList.length; i++){
-                    if(object.has(filterList[i])){
-                        count ++;
-                    }
-                }
-                if(count >= 1){
-                    return true;
+
+                for(int i = 0; i < allowedSeneorList.length; i++) {
+                    if (object.has(allowedSeneorList[i])) return true;
                 }
             }
-        }
-        return false;
+        // }
 
+        return false;
     }
 
     @Override
@@ -68,20 +67,14 @@ public class Preprocess implements Executable {
     
                 if (!messageQ.isEmpty()) {
                     JSONObject msg = messageQ.poll();
-
-                    // log.info(msg.toString());
                     
-                    boolean allowedTopic = enableTopic(Config.getCurrentConfig().getString("an"), msg.getString("topic"));
-                    boolean allowedSensor = enableSensor(Config.getCurrentConfig().getString("s"), msg);
-
-                    // log.info("{} / {}", allowedTopic, allowedSensor);
+                    // TODO config rename하면서 여기도 수정해야 함.
+                    boolean isAllowedTopic = enableTopic(Config.getCurrentConfig().getString("an"), msg.getString("topic"));
+                    boolean isAllowedSensor = enableSensor(Config.getCurrentConfig().getString("s"), msg.getJSONObject("payload"));
                     
-                    if (allowedTopic && allowedSensor) {
-                        // log.info(msg.getString("topic"));
+                    if (isAllowedTopic && isAllowedSensor) {
                         for (Wire outWire : outWires) {
-                            // log.info(msg.toString());
                             outWire.getBq().put(msg);
-                            // log.info(outWire.getBq().size()+"");
                         }
                     }
                 }
