c86f55def466376d948431008d96bd0bc1bdd9ca
jki12 <sjak0227@naver.com>
2023-12-03 15:54:21 +0900

    [fix] type 추가, toJson -> export로 이름 변경

--- src/main/java/com/nhnacademy/node/Node.java
+++ src/main/java/com/nhnacademy/node/Node.java
@@ -5,16 +5,30 @@ import java.util.UUID;
 import org.json.JSONObject;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public abstract class Node {
     private final UUID id;
     private final long createdTime;
+    private final String type;
+    private String name;
 
-    protected Node() {
+    protected Node(String type, String name) {
         this.id = UUID.randomUUID();
         this.createdTime = System.currentTimeMillis();
+
+        this.type = type;
+        this.name = name;
     }
 
-    abstract public JSONObject toJson();
+    public JSONObject export() {
+        JSONObject obj = new JSONObject();
+
+        obj.put("id", getId());
+        obj.put("type", getType());
+
+        return obj;
+    }
 }
