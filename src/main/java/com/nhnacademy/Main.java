fe7fa98233c96a4f6f181b5ef0e76b383f0fc8fe
jki12 <sjak0227@naver.com>
2023-12-03 19:15:00 +0900

    save

--- src/main/java/com/nhnacademy/Main.java
+++ src/main/java/com/nhnacademy/Main.java
@@ -5,68 +5,73 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
-import org.eclipse.paho.client.mqttv3.util.Debug;
+import org.json.JSONObject;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.Source;
 
 import com.nhnacademy.function.Postprocess;
 import com.nhnacademy.function.Preprocess;
+import com.nhnacademy.function.SayHello;
 import com.nhnacademy.node.DebugNode;
 import com.nhnacademy.node.FunctionNode;
 import com.nhnacademy.node.MqttInNode;
 import com.nhnacademy.node.MqttOutNode;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.script.*;
+
+@Slf4j
 public class Main {
-    private static void testProcess() {
-        Wire inTopre = new Wire();
-        Wire preToPost = new Wire();
-        Wire postToOut = new Wire();
-        Wire inToDebug = new Wire();
-        Wire preToDebug = new Wire();
-        Wire postToDebug = new Wire();
 
-        MqttInNode inNode = new MqttInNode();
-        FunctionNode preFunctionNode = new FunctionNode(new Preprocess(), "PreProcess");
-        FunctionNode postFunctionNode = new FunctionNode(new Postprocess(), "PostProcess");
-        MqttOutNode outNode = new MqttOutNode();
-        DebugNode intoDebugNode = new DebugNode();
-        DebugNode preToDebugNode = new DebugNode();
-        DebugNode postToDebugNode = new DebugNode();
+    private static void runJavascript() {
+        try (Engine engine = Engine.newBuilder()
+        .option("engine.WarnInterpreterOnly", "false")
+        .build()) {
 
-        // 정규 연결
-        inNode.wireOut(inTopre);
+            try (Context ctx = Context.newBuilder("js").engine(engine).build()) {
+                ctx.eval("js", "print('hello world');");
+            }
 
-        preFunctionNode.wireIn(inTopre);
-        preFunctionNode.wireOut(preToPost);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
 
-        postFunctionNode.wireIn(preToPost);
-        postFunctionNode.wireOut(postToOut);
+    private static void test() {
+        Wire w1 = new Wire();
+        Wire w2 = new Wire();
+        Wire w3 = new Wire();
+        Wire w4 = new Wire();
 
-        outNode.wireIn(postToOut);
+        MqttInNode in = new MqttInNode("in");
+        FunctionNode pre = new FunctionNode(new Preprocess(), "pre");
+        FunctionNode post = new FunctionNode(new Postprocess(), "post");
+        DebugNode debug = new DebugNode();
+        MqttOutNode out = new MqttOutNode();
 
-        // 디버그 연결
-        // MqttIn to debug
-        // inNode.wireOut(inToDebug);
-        // intoDebugNode.wireIn(inToDebug);
+        in.wireOut(w1);
+        pre.wireIn(w1);
 
-        // pre to debug
-        preFunctionNode.wireOut(preToDebug);
-        preToDebugNode.wireIn(preToDebug);
+        pre.wireOut(w2);
+        post.wireIn(w2);
 
-        // post to debug
-        // postFunctionNode.wireOut(postToDebug);
-        // postToDebugNode.wireIn(postToDebug);
+        post.wireOut(w3);
+        debug.wireIn(w3);
 
-        inNode.start();
-        preFunctionNode.start();
-        postFunctionNode.start();
-        outNode.start();
-        // intoDebugNode.start();
-        preToDebugNode.start();
-        // postToDebugNode.start();
+        post.wireOut(w4);
+        out.wireIn(w4);
 
+        in.start();
+        pre.start();
+        post.start();
+        debug.start();
+        out.start();
     }
 
     public static void main(String[] args) {
-        Options options = new Options();
+        // Options options = new Options();
 
         // TODO options의 설명 추가하기, help문 추가, 옵션에 없는 값이 들어올 경우 문제 try문 넣기.
         // options.addOption("c", false, "");
@@ -92,6 +97,6 @@ public class Main {
         // formatter.printHelp("mqtt [option] ... ", options);
         // }
 
-        testProcess();
+        test();
     }
 }
